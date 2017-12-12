a05dc17a045c2bbfc2dea2f8274e58833ab80c04
Mark Eschbach <mark@rhumbix.com>
2017-12-12 15:04:53 -0800

    [main] Document the es6/main functions

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
-//Promise Node Callback
+/**
+ * An adapter from the standard (error, result) NodeJS callback style.
+ *
+ * @callback perform Accepts a callback to interpret the success/callback of the NodeJS style
+ * @returns a promise to be resolved when the callback passed to perform function resolves.
+ */
 function es6_node( perform ) {
 	return new Promise( ( accept, reject ) => {
 		try {
@@ -16,6 +21,13 @@ function es6_node( perform ) {
 	})
 }
 
+/**
+ * Wraps the given operation and logs failures of the promise.  This is primarily intended to provide async entry points
+ * to avoid having to re-write the this for every command
+ *
+ * @callback perform An async function to be resolved or logged
+ * @param logger Logger to be passed to the performer and target when the application fails
+ */
 function main( perform, logger = console ) {
 	perform( logger )
 		.then(
