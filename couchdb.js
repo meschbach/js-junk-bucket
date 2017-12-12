36417abc7b88f6575310057ab29e2be9c2088d09
Mark Eschbach <mark@rhumbix.com>
2017-12-12 15:22:14 -0800

    [CouchDB] Documents  some of the methods at different levels of the heirarchy.

--- couchdb.js
+++ couchdb.js
@@ -1,19 +1,45 @@
 const nano = require( "nano" )
 const { es6_node } = require( './index' )
 
+/**
+ * A single CouchDB instance to communicate with over a well defined URL.  Exposes asynchronous management of database
+ * wide operations such as manipulating databases.
+ *
+ * @type CouchDB.Service
+ */
 class Service {
+	/**
+	 * Declares a new service to attempt to communicate with
+	 * @param host the URL of the CouchDB instnaces to be communciated with
+	 */
 	constructor( host = "http://localhost:5984" ){
 		this.client = nano( host )
 	}
 
+	/**
+	 * Creates a new database if the database doesn't exist.  If the database does exist it's considered an error
+	 *
+	 * @param name Name of the database to be created
+	 * @return {Promise} completed when the database is created
+	 * @throws Error if the database exxists
+	 */
 	create_db( name ) {
 		return es6_node( (cb ) => this.client.db.create( name, cb ) )
 	}
 
+	/**
+	 * Enumerates all databases within the represenetd CouchDB system
+	 * @return {Promise<Collection<String>>} the names of the database existing within the represented CouchDB service
+	 */
 	list_dbs(){
 		return es6_node( ( cb ) => this.client.db.list( cb ) )
 	}
 
+	/**
+	 * Creates a new database by the given name if it doesn't exist, otherwise returns a wrapper around the database
+	 *
+	 * @return {Promise<CouchDB.Database>} a promise for the database representation
+	 */
 	async ensure_db_exists( name ){
 		const dbs = await this.list_dbs()
 		if( !dbs.includes(name) ){
@@ -23,6 +49,9 @@ class Service {
 	}
 }
 
+/**
+ * Encapsulates the CouchDB operations.
+ */
 class Database {
 	constructor( client ) {
 		this.client = client
@@ -36,6 +65,13 @@ class Database {
 		return es6_node( ( cb ) => this.client.get( id, params, cb ) )
 	}
 
+	/**
+	 * Locates the given document by ID if it exists, otherwise return null
+	 *
+	 * @param id the _id property of the database to be resolved
+	 * @param params optionally additional parameters to send the CouchDB during this operation
+	 * @return {Promise<*>) A promise to resolve the document or null if the document doesn't exist.
+	 */
 	maybe_by_id( id, params ){
 		return es6_node( ( cb ) => {
 			this.client.get( id, params, ( err, data ) => {
@@ -66,6 +102,14 @@ class Database {
 		return await this.insert( modified )
 	}
 
+	/**
+	 * Updates the document if it exists or inserts the given document if the document doesn't exist
+	 *
+	 * @param id {String} the _id of the document to be modified
+	 * @param generator { function() = Promise } A promise gneerator to create a new document for insert
+	 * @param modfiied { function( originalDocument ) = Promise} a promise generator to modify the original document to be stored
+	 * @return the results fo the insert or update
+	 */
 	async upsert( id, generator, updater ){
 			const document = await this.get_by_id( id )
 			if( document ){
