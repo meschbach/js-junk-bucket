0533bfd641dd95eec1eeef5cc38d79c41923e861
Mark Eschbach <meschbach@gmail.com>
2018-08-11 11:55:49 -0700

    [logging/bunyan] App name is required for building the logger

--- logging-bunyan.js
+++ logging-bunyan.js
@@ -1,13 +1,16 @@
 const bunyan = require("bunyan");
 
-function formattedConsoleLog( opts = {} ){
+function formattedConsoleLog(appName, opts = {}) {
 	const bunyanFormat = require("bunyan-format");
+
+
 	const formattedLogger = bunyanFormat({outputMode: 'short'});
 
 	const bunyanOptions = Object.assign({
+		name: appName,
 		stream: formattedLogger,
 		level: debug
-	}, opts );
+	}, opts);
 	const rootLogger = bunyan.createLogger(bunyanOptions);
 	return rootLogger;
 }
