b7bb7323c38a6ddd0a11efbc2d8aab93b84d4302
Mark Eschbach <meschbach@gmail.com>
2018-12-30 21:07:33 -0800

    quality: Migrates from 'new Buffer' to 'Buffer.from'

--- tests/streams-tests.js
+++ tests/streams-tests.js
@@ -5,7 +5,7 @@ const {promisePiped} = require("../streams");
 describe("promisePiped", function(){
 	it( "pipes the full buffer through", async function(){
 		const istream = new stream.PassThrough();
-		istream.end(new Buffer('Test data.'));
+		istream.end(Buffer.from('Test data.'));
 		const ostream = new stream.PassThrough();
 		await promisePiped( istream, ostream );
 	} );
