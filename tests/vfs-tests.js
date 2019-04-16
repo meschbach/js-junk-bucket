1e6b42669b3e763e57ce7d021a5c4a5497e5969d
Mark Eschbach <meschbach@gmail.com>
2019-04-15 18:28:59 -0700

    vfs: Test additional VFS behaviors

--- tests/vfs-tests.js
+++ tests/vfs-tests.js
@@ -1,6 +1,14 @@
 const {expect} = require("chai");
 const {InMemoryVFS} = require("../vfs");
 const {MemoryWritable, promisePiped} = require("../streams");
+const {promiseEvent} = require("../future");
+
+async function promiseFinished( from, writeChunk ){
+	const done = promiseEvent( from, "finish");
+	const result = await writeChunk();
+	await done;
+	return result;
+}
 
 function abstractVFSBehavior( VFS, name ) {
 	describe(name + " acts like a VFS", function () {
@@ -19,26 +27,59 @@ function abstractVFSBehavior( VFS, name ) {
 				await this.sut.putBytes(this.fileName, this.fileContents, this.fileContents.encoding);
 			});
 
-			it("is readable as a stream", async function(){
-				const istream = await this.sut.createReadableStream(this.fileName);
-				//TODO: The following should really be abstracted into a "promise to read all bytes" thing
-				const sink = new MemoryWritable();
-				await promisePiped(istream,sink);
-				expect(sink.bytes.toString("utf-8")).to.deep.eq(this.fileContents);
+			abstractReadBackBehaviors();
+		});
+
+		describe("When creating a file as a stream", function(){
+			beforeEach(async function(){
+				this.sut = new VFS();
+				this.fileName = "mono-inc";
+				this.fileContents = "Is there somebody out here?  Is somebody listening?";
+				const sink = await this.sut.createWritableStream(this.fileName);
+				await promiseFinished(sink, async () => {
+					sink.end(this.fileContents);
+				});
 			});
 
-			it("is readable as bytes", async function(){
-				const bytes = await this.sut.asBytes(this.fileName);
-				expect(bytes.toString("utf-8")).to.deep.eq(this.fileContents);
-			});
-			it("can not mutate returned byte buffer", async function(){
-				const bytes1 = await this.sut.asBytes(this.fileName);
-				bytes1.writeUInt32LE(51);
-				const bytes2 = await this.sut.asBytes(this.fileName);
-				expect(bytes2.toString("utf-8")).to.deep.eq(this.fileContents);
-			});
+			abstractReadBackBehaviors();
 		});
 	});
 }
 
 abstractVFSBehavior(InMemoryVFS, "InMemoryVFS");
+
+function abstractReadBackBehaviors(){
+	it("is readable as a stream", async function(){
+		const istream = await this.sut.createReadableStream(this.fileName);
+		//TODO: The following should really be abstracted into a "promise to read all bytes" thing
+		const sink = new MemoryWritable();
+		await promisePiped(istream,sink);
+		expect(sink.bytes.toString("utf-8")).to.deep.eq(this.fileContents);
+	});
+
+	it("is readable as bytes", async function(){
+		const bytes = await this.sut.asBytes(this.fileName);
+		expect(bytes.toString("utf-8")).to.deep.eq(this.fileContents);
+	});
+
+	it("can not mutate returned byte buffer", async function(){
+		const bytes1 = await this.sut.asBytes(this.fileName);
+		bytes1.writeUInt32LE(42);
+		const bytes2 = await this.sut.asBytes(this.fileName);
+		expect(bytes2.toString("utf-8")).to.deep.eq(this.fileContents);
+	});
+
+	it("exists", async function(){
+		expect( await this.sut.exists(this.fileName)).to.eq(true);
+	});
+
+	describe("And is unlinked", function(){
+		beforeEach(async function(){
+			await this.sut.unlink(this.fileName);
+		});
+
+		it("does not exist", async function(){
+			expect(await this.sut.exists(this.fileName)).to.eq(false);
+		});
+	});
+}
