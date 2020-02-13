db2eb95b24cb79c49823f7d270ba56ef62a5f6da
Mark Eschbach <meschbach@gmail.com>
2020-02-12 16:08:48 -0800

    Generic filtering and aggregation metafunctions

--- fn.js
+++ fn.js
@@ -13,8 +13,47 @@ function arg2(_arg1, arg2){
 	return arg2;
 }
 
+function filterEnvelope(low,high, element){
+	return function(o){
+		const value = element(o);
+		return low < value &&  value <= high;
+	}
+}
+
+function y(...fns){
+	return function (input) {
+		return fns.reduce((value,fn) => fn(value), input);
+	}
+}
+
+function append(prototype) {
+	return function (input) {
+		return Object.assign({}, input, prototype);
+	}
+}
+
+function sortHigh( element ){
+	return function (lhs,rhs) {
+		return element(rhs) - element(lhs);
+	}
+}
+
+function above( threshold, element){
+	return function (input) {
+		return threshold < element(input);
+	}
+}
+
+function truthy(input){ return !!input }
+
 module.exports = {
+	above,
+	arg2,
+	append,
+	filterEnvelope,
 	identity,
 	property,
-	arg2
+	sortHigh,
+	truthy,
+	y
 };
